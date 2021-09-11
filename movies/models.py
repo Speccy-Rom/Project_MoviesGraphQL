aebeee45e681e1b4f32b18b69177a9e0b09ac8d9
Speccy-Rom <speccy.rom@gmail.com>
2021-09-11 23:25:02 +0300

    added: models actors and movies

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    """Актеры и режиссеры"""
+    name = models.CharField("Имя", max_length=100)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Movie(models.Model):
+    """Фильм"""
+    title = models.CharField("Название", max_length=100)
+    actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    description = models.TextField("Описание")
+    country = models.CharField("Страна", max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
