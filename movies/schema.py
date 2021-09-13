b3d73006655875c0f50d12f4fcb333db101dc9f4
Speccy-Rom <speccy.rom@gmail.com>
2021-09-13 23:04:16 +0300

    create: mutations for movies

--- movies/schema.py
+++ movies/schema.py
@@ -88,3 +88,59 @@ class UpdateActor(graphene.Mutation):
             actor_instance.save()
             return UpdateActor(ok=ok, actor=actor_instance)
         return UpdateActor(ok=ok, actor=None)
+
+
+# Create mutations for movies
+class CreateMovie(graphene.Mutation):
+    class Arguments:
+        input = MovieInput(required=True)
+
+    ok = graphene.Boolean()
+    movie = graphene.Field(MovieType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        actors = []
+        for actor_input in input.actors:
+            actor = Actor.objects.get(pk=actor_input.id)
+            if actor is None:
+                return CreateMovie(ok=False, movie=None)
+            actors.append(actor)
+        movie_instance = Movie(
+            title=input.title,
+            year=input.year
+        )
+        movie_instance.save()
+        movie_instance.actors.set(actors)
+        return CreateMovie(ok=ok, movie=movie_instance)
+
+
+class UpdateMovie(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = MovieInput(required=True)
+
+    ok = graphene.Boolean()
+    movie = graphene.Field(MovieType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        movie_instance = Movie.objects.get(pk=id)
+        if movie_instance:
+            ok = True
+            actors = []
+            for actor_input in input.actors:
+                actor = Actor.objects.get(pk=actor_input.id)
+                if actor is None:
+                    return CreateMovie(ok=False, movie=None)
+                actors.append(actor)
+            movie_instance = Movie(
+                title=input.title,
+                year=input.year
+            )
+            movie_instance.save()
+            movie_instance.actors.set(actors)
+            return UpdateMovie(ok=ok, movie=movie_instance)
+        return UpdateMovie(ok=ok, movie=None)
